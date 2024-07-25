b1a2391c020e48f790f033093a2d4aa055bacae6
Thibaud CANALE <thican@thican.net>
2024-07-25 21:03:32 +0200

    init.lua: adds personal modifications
    Signed-off-by: Thibaud CANALE <thican@thican.net>

--- init.lua
+++ init.lua
@@ -91,7 +91,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
+
+-- Activate kickstart.nvim’s health plugin
+require 'health'
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
