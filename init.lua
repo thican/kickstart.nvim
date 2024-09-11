01cb9b5f21cd6e1be4433ab2fad9d4f5256ed346
Thibaud CANALE <thican@thican.net>
2024-09-11 22:12:05 +0200

    feat: disables sharing clipboard with OS
    OHMAGAD!! Worst idea EVAAAAARR!!!11 KILL IT LIKE SCORCHED EARTH!
    
    Signed-off-by: Thibaud CANALE <thican@thican.net>

--- init.lua
+++ init.lua
@@ -127,7 +127,9 @@ do
   --  Schedule the setting after `UiEnter` because it can increase startup-time.
   --  Remove this option if you want your OS clipboard to remain independent.
   --  See `:help 'clipboard'`
+  --[[
   vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+  --]]
 
   -- Enable break indent
   vim.o.breakindent = true
