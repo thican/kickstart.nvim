47dc3a054292c93478f14ecc75125506bef1b62c
Thibaud CANALE <thican@thican.net>
2024-12-30 23:09:48 +0100

    feat: adds LSP clangd and rust_analyzer
    Signed-off-by: Thibaud CANALE <thican@thican.net>

--- init.lua
+++ init.lua
@@ -610,10 +610,23 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       ---@type table<string, vim.lsp.Config>
       local servers = {
-        -- clangd = {},
+        clangd = {
+          cmd = {
+            'clangd',
+            '--compile-commands-dir=builddir',
+            '--background-index',
+            '--background-index-priority=low',
+            '--clang-tidy',
+            '--completion-style=detailed',
+            '--header-insertion=iwyu',
+            '--enable-config',
+            '--log=verbose',
+          },
+        },
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+        },
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
