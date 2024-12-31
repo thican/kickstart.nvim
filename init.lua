031b3ad930071d8d33f523825f6c2b92c178cf9c
Thibaud CANALE <thican@thican.net>
2024-12-31 06:57:40 +0100

    feat: adds sitter 'just'
    https://github.com/casey/just
    
    Signed-off-by: Thibaud CANALE <thican@thican.net>

--- init.lua
+++ init.lua
@@ -927,7 +927,10 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = {
+    'bash', 'c', 'diff', 'html', 'just', 'lua', 'luadoc',
+    'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc',
+  }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
